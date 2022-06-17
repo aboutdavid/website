be4285a67ccbb7b05121cb74fc2db850c7dc0f6b
aboutDavid <62346025+aboutDavid@users.noreply.github.com>
2022-06-17 14:35:18 -0400

    Wait for ackee to fully load before sending events

--- analytics.js
+++ analytics.js
@@ -1,27 +1,29 @@
 var domainID = "ed2d6c15-7aca-42fc-8f8f-6c0a49fed42e"
-var instance = ackeeTracker.create('https://ackee.aboutdavid.me/')
-function clickedOnPost(){
-instance.action(domainID, {
-	key: 'Blog Post Viewed',
-	value: 1
-})
-}
-function clickedOnProject(){
-    instance.action(domainID, {
-        key: 'Project Viewed',
-        value: 1
-    })
-}
-function clickedOnTranslationsGithub(){
-    instance.action(domainID, {
-        key: 'GitHub Translation Page Viewed',
-        value: 1
-    })
-}
-function clickedOnTranslations(file){
-    var lang = file.replace(".json","")
-    instance.action(domainID, {
-        key: `User set language to ${lang}`,
-        value: 1
-    })
-}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    window.clickedOnPost = function() {
+        ackeeTracker.create('https://ackee.aboutdavid.me/').action(domainID, {
+            key: 'Blog Post Viewed',
+            value: 1
+        })
+    }
+    window.clickedOnProject = function() {
+        ackeeTracker.create('https://ackee.aboutdavid.me/').action(domainID, {
+            key: 'Project Viewed',
+            value: 1
+        })
+    }
+    window.clickedOnTranslationsGithub = function() {
+        ackeeTracker.create('https://ackee.aboutdavid.me/').action(domainID, {
+            key: 'GitHub Translation Page Viewed',
+            value: 1
+        })
+    }
+    window.clickedOnTranslations = function(file) {
+        var lang = file.replace(".json", "")
+        ackeeTracker.create('https://ackee.aboutdavid.me/').action(domainID, {
+            key: `Language: ${lang}`,
+            value: 1
+        })
+    }
+});
