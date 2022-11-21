d59388be2d3990f9ccbe089da1facb119e79d3b6
aboutdavid <aboutdavid@protonmail.com>
2022-11-20 22:37:10 -0500

    add latest github commit to website

---
+++ _data/commit.js
@@ -0,0 +1,31 @@
+const username = "aboutDavid";
+var request = require("sync-request");
+var res = request(
+  "GET",
+  `https://api.github.com/users/${username}/events/public`,
+  {
+    headers: {
+      "user-agent": "David's Website",
+      //Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
+    }
+  }
+);
+function time(date) {
+return new Date(date).toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: "short", dateStyle: "long" }).toString()
+}
+const activities = JSON.parse(res.getBody());
+
+for (let i = 0; i < activities.length; i++) {
+  if (activities[i].type === "PushEvent") {
+    const commit =
+      activities[i].payload.commits[activities[i].payload.commits.length - 1];
+
+    module.exports = {
+      message: commit.message,
+      url: `https://github.com/${activities[i].repo.name}/commit/${commit.sha}`,
+      time: time(activities[i].created_at),
+      repo: activities[i].repo.name
+    };
+    break;
+  }
+}
