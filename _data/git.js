9496091bc8b0075f47ab1b790f34df9f5e913cdc
aboutdavid <aboutdavid@protonmail.com>
2022-11-20 21:16:05 -0500

    fix date + primary lang on projects

--- _data/git.js
+++ _data/git.js
@@ -1,25 +1,6 @@
-var units = {
-    year  : 24 * 60 * 60 * 1000 * 365,
-    month : 24 * 60 * 60 * 1000 * 365/12,
-    day   : 24 * 60 * 60 * 1000,
-    hour  : 60 * 60 * 1000,
-    minute: 60 * 1000,
-    second: 1000
-  }
-  
-  var rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
-  
-  var getRelativeTime = (d1, d2 = new Date()) => {
-    var elapsed = d1 - d2
-  
-    // "Math.abs" accounts for both "past" & "future" scenarios
-    for (var u in units) 
-      if (Math.abs(elapsed) > units[u] || u == 'second') 
-        return rtf.format(Math.round(elapsed/units[u]), u)
-  }
 var int = parseInt(require('child_process').execSync('git log -1 --format=%ct').toString())
 module.exports = {
     full: require('child_process') .execSync('git rev-parse HEAD') .toString().trim(),
     short: require('child_process') .execSync('git rev-parse --short HEAD') .toString().trim(),
-    time: getRelativeTime(int * 1000)
+    date: new Date(int * 1000).toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: "short", dateStyle: "long" })+ " (EST)"
 }
